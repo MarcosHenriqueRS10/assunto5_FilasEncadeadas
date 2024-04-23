96d90aed977864a7734342241e512658d6539d23
Marcos Henrique Ribeiro de Santana <161729448+MarcosHenriqueRS10@users.noreply.github.com>
2024-04-23 09:37:41 -0300

    Add files via upload

---
+++ exemplo/MainTeste.java
@@ -0,0 +1,21 @@
+package exemplo;
+
+import filas.FilaInt;
+
+public class MainTeste {
+
+	public static void main(String[] args) {
+		FilaInt fila = new FilaInt();
+		fila.init();
+		
+		fila.enqueue(23);
+		fila.enqueue(44);
+		fila.enqueue(38);
+		
+		System.out.println(fila.dequeue());
+		System.out.println(fila.dequeue());
+		System.out.println(fila.dequeue());
+
+	}
+
+}
